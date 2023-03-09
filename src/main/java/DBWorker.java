d56dbea9c56190be37276460dae13a03114fd79a
stas87p <yhoho1987@gmail.com>
2023-03-09 15:57:02 +0300

    сука блядская

--- src/main/java/DBWorker.java
+++ src/main/java/DBWorker.java
@@ -1,2 +1,20 @@
-public class Connect {
+import java.sql.*;
+
+public class DBWorker {
+    private final String HOST = "jdbc:postgresql://localhost:5432/test";
+    private final String USERNAME = "postgres";
+    private final String PASSWORD = "Serialbas1";
+
+    private Connection connection;
+
+    public DBWorker()  {
+        try {
+            connection = DriverManager.getConnection(HOST, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,29 @@
-public class user {
+public class User {
+
+    private int id;
+    private String username;
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+
+    }
+
+    public User(int id, String username, String password) {
+
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+    public int getId() { return id; }
+    public void getId(int id) { this.id = id;}
+    public String getUsername() { return username; }
+    public void getUsername(String username) {this.username = username; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
